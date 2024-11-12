6991c50c580f30488a4af5e1be707ea270310253
Enor Cerna <u17200479@utp.edu.pe>
2024-11-12 05:20:45 -0500

    index route

---
+++ app/routes/_index.tsx
@@ -0,0 +1,23 @@
+import type {MetaFunction} from "@remix-run/node";
+import {useLoaderData} from "@remix-run/react";
+import H2 from "~/components/pages/H2";
+import ListPosters from "~/components/pages/ListPosters";
+import {Client} from "~/services/api";
+
+export const meta: MetaFunction = () => {
+  return [
+    {title: "Redrama - Tus series favoritas"},
+    {name: "description", content: "Welcome to Remix!"}
+  ];
+};
+export const clientLoader = async () => {
+  return await Client.getlastDramas();
+};
+export default function Index() {
+  return (
+    <div className="flex  flex-col p-3">
+      <H2>Ultimos Kramas</H2>
+      <ListPosters items={useLoaderData()} />
+    </div>
+  );
+}
