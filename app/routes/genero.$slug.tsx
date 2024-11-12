b536bff3a83dad65da2879ceb3dc45ef713b6d1e
Enor Cerna <u17200479@utp.edu.pe>
2024-11-12 05:21:13 -0500

    gener slug route

---
+++ app/routes/genero.$slug.tsx
@@ -0,0 +1,35 @@
+import {LoaderFunctionArgs} from "@remix-run/node";
+import {MetaFunction, useLoaderData} from "@remix-run/react";
+import PaginateDorama from "~/components/pages/dorama/PaginateDorama";
+import ListPosters from "~/components/pages/ListPosters";
+import {CONFIG} from "~/constants/main";
+import {Client} from "~/services/api";
+
+export const clientLoader = async ({params, request}: LoaderFunctionArgs) => {
+  const gre = await Client.getGenreBySlug(params.slug as string);
+  const url = new URL(request.url);
+  const page = url.searchParams.get("page");
+  const pagination = await Client.getPaginationDramasByGenreId(
+    gre._id,
+    page as string
+  );
+  return {gre, pagination};
+};
+export const meta: MetaFunction<typeof clientLoader> = ({data}) => {
+  return [{title: `${data?.gre.name} | ${CONFIG.Name}`}];
+};
+function GenreSlug() {
+  const {gre, pagination} = useLoaderData<typeof clientLoader>();
+  return (
+    <article className="p-3 flex-col flex mb-2 gap-3">
+      <h1 className="text-center text-3xl mb-4">{gre.name}</h1>
+      <ListPosters items={pagination.items} />
+      <PaginateDorama
+        count={pagination.count}
+        pageInfo={pagination.pageInfo}
+      />
+    </article>
+  );
+}
+
+export default GenreSlug;
